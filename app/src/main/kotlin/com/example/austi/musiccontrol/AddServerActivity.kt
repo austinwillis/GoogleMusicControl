2bc8229878a1c67d7acc643bd01bdc139b9134c7
Austin Willis <austinwillis8@gmail.com>
2016-06-11 23:35:56 -0500

    AddServerActivity added and pull to refresh opens example list of cities

--- app/src/main/kotlin/com/example/austi/musiccontrol/AddServerActivity.kt
+++ app/src/main/kotlin/com/example/austi/musiccontrol/AddServerActivity.kt
@@ -1,16 +1,85 @@
 package com.example.austi.musiccontrol
 
+import android.graphics.Color
 import android.os.Bundle
+import android.os.Handler
+import android.support.v4.widget.SwipeRefreshLayout
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+import java.util.Arrays
 
-class AddServerActivity : AppCompatActivity() {
+class AddServerActivity : AppCompatActivity(), SwipeRefreshLayout.OnRefreshListener {
+
+
+    private var swipeView: SwipeRefreshLayout? = null
+
+    private var listView: ListView? = null
+    private var adapter: ArrayAdapter<String>? = null
+
+    private val LIST_ITEM_TEXT_CITIES = arrayOf("Los Angeles", "Chicago", "Indianapolis", "San Francisco", "Oklahoma City", "Washington")
+
+    private val LIST_ITEM_TEXT_MORE_CITIES = arrayOf("Phoenix", "San Antonio", "San Jose", "Nashville", "Las Vegas", "Virginia Beach")
+
+    private var cityList: List<String>? = null
+
+    // variable to toggle city values on refresh
+    internal var refreshToggle = true
+    private var toolbar: Toolbar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.serverlist)
-        val addToolbar = findViewById(R.id.add_server_toolbar) as Toolbar?
-        setSupportActionBar(addToolbar)
-        supportActionBar!!.setHomeButtonEnabled(true)
+        toolbar = findViewById(R.id.add_server_toolbar) as Toolbar?
+        setSupportActionBar(toolbar)
+
+        swipeView = findViewById(R.id.swipe_view) as SwipeRefreshLayout?
+        swipeView!!.setOnRefreshListener(this)
+        swipeView!!.setColorSchemeColors(Color.GRAY, Color.GREEN, Color.BLUE,
+                Color.RED, Color.CYAN)
+        swipeView!!.setDistanceToTriggerSync(20)// in dips
+        swipeView!!.setSize(SwipeRefreshLayout.DEFAULT)// LARGE also can be used
+
+        cityList = Arrays.asList(*LIST_ITEM_TEXT_CITIES)
+        listView = findViewById(R.id.list) as ListView?
+        adapter = ArrayAdapter(applicationContext,
+                R.layout.list_item, cityList!!)
+        listView!!.adapter = adapter
+        listView!!.requestLayout()
     }
+
+    internal var handler: Handler = object : Handler() {
+        override fun handleMessage(msg: android.os.Message) {
+
+            if (refreshToggle) {
+                refreshToggle = false
+                cityList = Arrays.asList(*LIST_ITEM_TEXT_MORE_CITIES)
+                adapter = ArrayAdapter(applicationContext,
+                        R.layout.list_item, cityList!!)
+            } else {
+                refreshToggle = true
+                cityList = Arrays.asList(*LIST_ITEM_TEXT_CITIES)
+                adapter = ArrayAdapter(applicationContext,
+                        R.layout.list_item, cityList!!)
+            }
+            listView!!.adapter = adapter
+
+            swipeView!!.postDelayed({
+                Toast.makeText(applicationContext,
+                        "city list refreshed", Toast.LENGTH_SHORT).show()
+                swipeView!!.isRefreshing = false
+            }, 1000)
+        }
+    }
+
+    override fun onRefresh() {
+
+        swipeView!!.postDelayed({
+            swipeView!!.isRefreshing = true
+            handler.sendEmptyMessage(0)
+        }, 1000)
+    }
+
 }
--- app/src/main/kotlin/com/example/austi/musiccontrol/MainActivity.kt
+++ app/src/main/kotlin/com/example/austi/musiccontrol/MainActivity.kt
@@ -5,16 +5,12 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.design.widget.FloatingActionButton
 import android.support.design.widget.NavigationView
-import android.support.design.widget.Snackbar
 import android.support.v4.view.GravityCompat
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
-import android.view.View
-import android.widget.ArrayAdapter
-import android.widget.ListView
 import com.google.android.gms.common.api.GoogleApiClient
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
@@ -32,7 +28,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         setSupportActionBar(myToolbar)
 
         val fab = findViewById(R.id.fab) as FloatingActionButton?
-        fab!!.setOnClickListener { view -> Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG).setAction("Action", null).show() }
+        fab!!.setOnClickListener { switchToAddServer() }
 
         val drawer = findViewById(R.id.drawer_layout) as DrawerLayout?
         val toggle = ActionBarDrawerToggle(
@@ -80,7 +76,7 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         }
     }
 
-    fun switchToAddServer(view: View) {
+    fun switchToAddServer() {
         val intent = Intent(this, AddServerActivity::class.java)
         startActivity(intent)
     }
@@ -90,18 +86,14 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         // Handle navigation view item clicks here.
         val id = item.itemId
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        if (id == R.id.nav_add_server) {
+            switchToAddServer()
+        } else if (id == R.id.nav_server_settings) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.nav_preferences) {
+            val intent = Intent(applicationContext, SettingsActivity::class.java)
+            startActivity(intent)
+            return true
         }
 
         val drawer = findViewById(R.id.drawer_layout) as DrawerLayout?
